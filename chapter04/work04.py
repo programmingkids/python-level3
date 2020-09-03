2de47bf18f3b4f202b46b422061dafa2d00c8f36
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2020-09-03 09:55:14 +0000

    fix comment on tuple

--- chapter04/work04.py
+++ chapter04/work04.py
@@ -2,22 +2,22 @@
 colors1 = ("red","blue","green","yellow","pink","black")
 print(colors1)
 
-# ['red', 'blue', 'green']
+# ('red', 'blue', 'green')
 colors2 = colors1[0:3]
 print(colors2)
 
-# ['green', 'yellow']
+# ('green', 'yellow')
 colors3 = colors1[2:4]
 print(colors3)
 
-# ['yellow', 'pink', 'black']
+# ('yellow', 'pink', 'black')
 colors4 = colors1[3:]
 print(colors4)
 
-# ['red', 'green', 'pink']
+# ('red', 'green', 'pink')
 colors5 = colors1[::2]
 print(colors5)
 
-# ['black', 'pink', 'yellow', 'green', 'blue', 'red']
+# ('black', 'pink', 'yellow', 'green', 'blue', 'red')
 colors6 = colors1[::-1]
 print(colors6)

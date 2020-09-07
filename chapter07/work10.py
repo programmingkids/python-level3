985d99227f447f8dbdabb7386555c328dd2d1397
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2020-09-07 10:18:11 +0000

    fix chapter07 comment

--- chapter07/work10.py
+++ chapter07/work10.py
@@ -1,5 +1,5 @@
 numbers = [3,5,6,2,9,7]
 
-# マップ関数とラムダ関数の呼び出し
+# マップ関数とラムダ式の呼び出し
 result = list(map(lambda n : n * 2, numbers))
 print(result)
--- chapter07/work11.py
+++ chapter07/work11.py
@@ -1,5 +1,5 @@
 numbers = [3,5,6,2,9,7]
 
-# フィルタ関数とラムダ関数の呼び出し
+# フィルタ関数とラムダ式の呼び出し
 result = list(filter(lambda n : n % 2 != 0, numbers))
 print(result)
